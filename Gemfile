5c6c078cf69c9e2f0dc7ec0cc30d506bb36fe4c6
asnganje <abd.nganje@gmail.com>
2025-03-30 10:42:00 +0300

    fix lint errors

--- Gemfile
+++ Gemfile
@@ -62,4 +62,4 @@ group :test do
   gem "selenium-webdriver"
 end
 
-gem 'dotenv-rails', groups: [:development, :test]
+gem "dotenv-rails", groups: [ :development, :test ]
--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require 'dotenv/load' if Rails.env.development? || Rails.env.test?
+require "dotenv/load" if Rails.env.development? || Rails.env.test?
 
 module MBlog
   class Application < Rails::Application
